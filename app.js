ad5e949e890a048a48a3340129d2c5a9ece4a01f
Beatriz <beatrizsouzasantos1288@gmail.com>
2024-01-18 17:21:00 -0300

    adicionando tentativas

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
-prompt('Escolha um número enre 1 e 30');
+let numeroSecreto = 5;
+console.log = (numeroSecreto);
+let selectedNumber;
+let tentativas = 1;
+
+while (selectedNumber != numeroSecreto){
+    selectedNumber = prompt('Escolha um número enre 1 e 10');
+
+    if (selectedNumber == numeroSecreto){
+        alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (selectedNumber > numeroSecreto) {
+            alert(`O número secreto é menor que ${selectedNumber}`);
+        } else {
+            alert(`O número secreto é maior que ${selectedNumber}`);
+        }
+        tentativas++;
+    }
+}
